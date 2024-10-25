d7c14dcb77f06a320fd5851169f46f6f266779a1
Weerawi <weerawikramatharindu@gmail.com>
2024-10-25 12:19:39 +0530

    add treatmentplan save

--- Controller/treatmentplan.controller.js
+++ Controller/treatmentplan.controller.js
@@ -1,6 +1,6 @@
 import { validationResult } from "express-validator";
 import ResponseMessages from "../config/messages.js";
-import { EntityId } from "../utils/type-def.js";
+import { DateString, EntityId, StringValue } from "../utils/type-def.js";
 import executeSp from "../utils/exeSp.js";
 import handleResponse from "../utils/handleResponse.js";
 import handleError from "../utils/handleError.js";
@@ -48,6 +48,90 @@ const TreatmentPlanController = {
             200,
             "success",
             "Data retrieved Successfully",
+            treatmentPlanHistoryGetResult
+        );
+        } catch (error) {
+        handleError(
+            response,
+            500,
+            "error",
+            error.message,
+            "Something went wrong"
+        );
+        next(error);
+        }
+    },
+
+
+    /**
+   *
+   * get medical certificate
+   *
+   * @param {request} request object
+   * @param {response} response object
+   * @param {next} next - middleware
+   * @returns
+   */
+    async saveTreatmentPlan(request, response, next) {
+        const errors = validationResult(request);
+        if (!errors.isEmpty()) {
+        return response.status(422).json({
+            error: true,
+            message: ResponseMessages.TreatmentPlan.VALIDATION_ERROR,
+            data: errors,
+        });
+        }
+
+        try {
+        let connection = request.app.locals.db;
+        
+        const { 
+            Id,
+            TeethId, 
+            TreatmentPlanName, 
+            Reason,
+            StartDate,
+            EstimatedDate,
+            Status, 
+            PatientId,
+            DoctorId, 
+            InstituteBranchId,
+            InstituteId,
+            UniqueId,
+        Info,
+        UserModified} = request.body;
+
+        var params = [
+            EntityId({ fieldName: "Id", value: Id }),
+            EntityId({ fieldName: "TeethId", value: TeethId }),
+            StringValue({ fieldName: "TreatmentPlanName", value: TreatmentPlanName }),
+            StringValue({ fieldName: "Reason", value: Reason }),
+            DateString({ fieldName: "StartDate", value: StartDate }),
+            DateString({ fieldName: "EstimatedDate", value: EstimatedDate }),
+            StringValue({ fieldName: "Status", value: Status }),
+            EntityId({ fieldName: "PatientId", value: PatientId }),
+            EntityId({ fieldName: "DoctorId", value: DoctorId }),
+            EntityId({ fieldName: "InstituteBranchId", value: InstituteBranchId }),
+            EntityId({ fieldName: "InstituteId", value: InstituteId }),
+            EntityId({ fieldName: "UniqueId", value: UniqueId }),
+            EntityId({ fieldName: "UserModified", value: UserModified }),
+            StringValue({ fieldName: "Info", value: Info }),
+        ];
+
+        let treatmentPlanHistoryGetResult = await executeSp({
+            spName: `TreatmentPlanSave`,
+            params: params,
+            connection,
+        });
+
+        treatmentPlanHistoryGetResult =
+            treatmentPlanHistoryGetResult.recordsets[0];
+
+        handleResponse(
+            response,
+            200,
+            "success",
+            "Data save Successfully",
             treatmentPlanHistoryGetResult
         );
         } catch (error) {
--- routes/treatmentplan.route.js
+++ routes/treatmentplan.route.js
@@ -14,4 +14,27 @@ router.post(
     TreatmentPlanController.getTreatmentPlanHistory
 )
 
+
+router.post(
+    "/TreatmentPlanSave",
+    [
+        check("Id").isInt().not().isEmpty(),
+        check("TeethId").isInt().not().isEmpty(),
+        check("TreatmentPlanName").isString().not().isEmpty(),
+        check("Reason").isString().not().isEmpty(),
+        check("StartDate").isDate().not().isEmpty(),
+        check("EstimatedDate").isDate().not().isEmpty(),
+        check("Status").isString().not().isEmpty(),
+        check("PatientId").isInt().not().isEmpty(),
+        check("DoctorId").isInt().not().isEmpty(),
+        check("InstituteBranchId").isInt().not().isEmpty(),
+        check("InstituteId").isInt().not().isEmpty(),
+        check("UniqueId").isInt().not().isEmpty(),
+        check("UserModified").isInt().not().isEmpty(),
+        check("Info").isString().not().isEmpty(),
+    ]
+    ,
+    TreatmentPlanController.saveTreatmentPlan
+)
+
 export default router;
